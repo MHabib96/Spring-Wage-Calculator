ab7667718f589a1de9257d1b98a309d46a4647b8
MHabib96 <33306932+MHabib96@users.noreply.github.com>
2018-07-29 21:20:05 -0700

    Add files via upload

---
+++ SpringEmployee/SpringEmployeeCW/src/main/java/com/springemployeecw/repositories/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.springemployeecw.repositories;
+
+import com.springemployeecw.models.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+}
